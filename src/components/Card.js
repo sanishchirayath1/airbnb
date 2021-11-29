afb966093a2a1c9d3d0b3c4af8a52759be92711e
sanishchirayath1 <sanishchirayath@gmail.com>
2021-11-29 18:50:03 +0530

    alts added

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} className="card--image" />
+            <img src={`../images/${props.img}`} className="card--image" alt=""/>
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src="../images/star.png" className="card--star" alt=""/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
